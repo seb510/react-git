1d51bd9f9c52a82c5f87a807f5cc552726b4dcb4
seb510 <sebistian@ukr.net>
2020-10-10 14:10:27 +0300

    change color text and add new classes in Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,14 +3,23 @@ import classes from './Profile.module.css';
 
 function Profile() {
     return (
-        <div className={classes.content}>
+      <div className={classes.content}>
         <div>
           <img src='https://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images.jpg' className={classes.back}/>
-        </div>  
-        <div className={classes.ava}>
-          <img src='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'/>
-          discription
-        </div>
+        </div> 
+        <div className={classes.inform}>
+          <div className={classes.ava}>
+            <img src='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'/>
+          </div>
+          <div>
+              <p>
+                Sebistian Mykola
+                <span>
+                  19 Julay 1992 year
+                </span>
+              </p>
+            </div>
+          </div> 
         <div>
           My post
           <div>
