bfd0fbc0ca075b9f57deedb5cf191d439791f781
seb510 <sebistian@ukr.net>
2020-10-13 19:17:24 +0300

    change file

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ function Post(props) {
           <img src="https://static.wikia.nocookie.net/avatar/images/0/09/3%D1%8515_%D0%9C%D1%8D%D0%B9_%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82.jpg/revision/latest/top-crop/width/360/height/450?cb=20110327130225&path-prefix=ru" alt="Avatar"/>
           <span>{props.message}</span>
           <p>
-            Like
+            Like -
             <span>{ props.count }</span>
             </p>
         </div>
