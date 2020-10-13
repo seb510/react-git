9611c8cb93356bc7f13fcfca4100bef6f63bfbb1
seb510 <sebistian@ukr.net>
2020-10-13 19:06:43 +0300

    add span in Post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,8 +5,12 @@ function Post(props) {
     return (
         <div className={classes.item}>
           <img src="https://static.wikia.nocookie.net/avatar/images/0/09/3%D1%8515_%D0%9C%D1%8D%D0%B9_%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82.jpg/revision/latest/top-crop/width/360/height/450?cb=20110327130225&path-prefix=ru" alt="Avatar"/>
-          {props.message}
-          <p>Like</p>{props.count}
+          <span>{props.message}</span>
+          <p>
+            Like
+            
+            <span>{props.count}</span>
+            </p>
         </div>
     );
 };
