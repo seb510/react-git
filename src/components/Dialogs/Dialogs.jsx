a6ab8fdcd1b120aa02538797aed8d4a8cce7e03e
seb510 <sebistian@ukr.net>
2020-10-15 15:34:57 +0300

    createng dialog with grid

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div >
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Dmitriy
+                </div>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Svitlana
+                </div>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog}>
+                    Victor
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Hi!
+                </div>
+                <div className={classes.message}>
+                    How are your dill my Friend?
+                </div>
+                <div className={classes.message}>
+                    Thanks everything is ok! and you?
+                </div>
+            </div>
         </div>
     )
 }
