ccaff4f47bf9054c3cf9379c316c3bb18e0469b3
seb510 <sebistian@ukr.net>
2020-10-19 14:36:02 +0300

    add Database for dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -22,21 +22,37 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
+    let  dialogsData = [
+        {id: 1, name: 'Dmitriny'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Svitlana'},
+        {id: 4, name: 'Victor'},
+        {id: 5, name: 'Vera'},
+        {id: 6, name: 'Sasha'},
+    ]
+
+    let  messageData = [
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Thanks. How are your dill my Friend?'},
+        {id: 4, message: 'Thanks everything is ok and you?'},
+        {id: 4, message: 'YOUUu'},
+    ]
+
+
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogsItem name='Dmitriy' id='1'/>
-                <DialogsItem name='Andrey' id='2'/>
-                <DialogsItem name='Svitlana' id='3'/>
-                <DialogsItem name='Victor' id='4'/>
-                <DialogsItem name='Vera' id='5'/>
-                <DialogsItem name='Sasha' id='6'/>
+                <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id}/>
             </div>
             <div className={classes.messages}>
-                <Message message='H!'/>
-                <Message message='How are your dill my Friend?'/>
-                <Message message='Thanks everything is ok! and you?'/>
-                <Message message='YOU?'/>
+                <Message message={messageData[0].message}/>
+                <Message message={messageData[1].message}/>
+                <Message message={messageData[2].message}/>
+                <Message message={messageData[3].message}/>
+                <Message message={messageData[4].message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 function MyPosts() {
+
+    let  postData = [
+        {id: 1, message: 'Hi! how are you?', likesCount: 12},
+        {id: 2, message: 'Hi! It is my first post', likesCount: 15},
+    ];
+
     return (
     <div className={classes.posts}>
       <h3>MY POST</h3>
@@ -15,8 +21,8 @@ function MyPosts() {
           </div>
       </div>
       <div className={classes.postsDialog}>
-        <Post message='Hi! how are you?' count='20'/>
-        <Post message='Hi! It is my first post' count='10'/>
+        <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+        <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
       </div>
     </div>
     );
