4f452f7f4429904259e9cceb6ee26d8b1314d9d8
seb510 <sebistian@ukr.net>
2020-10-21 16:54:08 +0300

    phone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <header className={classes.header}>
             <img src='https://www.georgiapower.com/content/georgia-power/en/company/media-resources/logos/_jcr_content/ifooter/columnctr/colTwo/columnctr_copy/colTwo/adaptiveimage.img.320.low.png/1516888423050.png'/>
-            <span className={classes.phone}>+380 (097) 236-586-22</span>
+            <span className={classes.phone}>+380 (097)236-586-22</span>
         </header>
     );
 };
